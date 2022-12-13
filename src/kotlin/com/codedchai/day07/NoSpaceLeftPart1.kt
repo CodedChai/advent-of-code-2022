c5b17388072b6c4d78b2b331ce5cc6d84e08d2df
CodedChai <guardianx515@gmail.com>
2022-12-12 21:59:51 -0600

    Advent of code day 7: part 1 - Directory handling

---
+++ src/kotlin/com/codedchai/day07/NoSpaceLeftPart1.kt
@@ -0,0 +1,117 @@
+package com.codedchai.day07
+
+import java.io.File
+
+class NoSpaceLeftPart1 {
+  fun findTotalDirSizeForDirsUnder100k() {
+    val lines = File("resources/day07/input.txt").readLines()
+
+    val directoriesMap = mutableMapOf<String, Directory>()
+    var currentDirectory: Directory? = null
+
+    lines.forEachIndexed { index, s ->
+      println(s)
+      currentDirectory = when {
+        s.isCommand() -> executeCommand(s, currentDirectory, directoriesMap)
+        s.isFile() && currentDirectory != null -> addFile(currentDirectory!!, s)
+        s.isDir() && currentDirectory != null -> addDir(currentDirectory!!, s)
+        else -> throw RuntimeException("Unidentified: $s")
+      }.also { println(it) }
+      directoriesMap[currentDirectory!!.path] = currentDirectory!!
+      // add all child dirs to this too
+      currentDirectory!!.childDirectories.forEach {
+        directoriesMap[it.path] = it
+      }
+    }
+
+    directoriesMap.toSortedMap().forEach { println(it) }
+  }
+
+  fun addFile(currentDirectory: Directory, fileDetails: String): Directory {
+    val (fileSize, fileName) = fileDetails.split(" ")
+    val newFile = AdventFile(fileName = fileName, size = fileSize.toInt())
+    currentDirectory.files.add(newFile)
+    return currentDirectory
+  }
+
+  fun addDir(currentDirectory: Directory, dirDetails: String): Directory {
+    val dirName = dirDetails.split(" ")[1]
+    val newDir = Directory(dirName = dirName, path = "${currentDirectory.path}$dirName/")
+    currentDirectory.childDirectories.add(newDir)
+    return currentDirectory
+  }
+
+  fun executeCommand(line: String, currentDirectory: Directory?, directoriesMap: Map<String, Directory>): Directory {
+    return if (line.startsWith("$ cd")) {
+      executeCd(line.split(" ")[2], currentDirectory, directoriesMap)
+    } else {
+      executeLs(currentDirectory!!)
+    }
+  }
+
+  fun executeLs(currentDirectory: Directory): Directory {
+    return currentDirectory // nop, nothing to do for this command in our case
+  }
+
+  fun executeCd(param: String, currentDirectory: Directory? = null, directoriesMap: Map<String, Directory>): Directory {
+    if (currentDirectory == null) {
+      return Directory(dirName = param, path = param)
+    }
+
+    if (param == "..") {
+      val currentPath = currentDirectory.path
+      val currentPathParts = currentPath.split("/").filterNot { it.isBlank() }
+      val newDir = currentPathParts.subList(0, currentPathParts.size - 1).joinToString("/")
+        .setRoot().addLeadingSlash().addTrailingSlash()
+      return directoriesMap[newDir]!!
+    }
+
+    return currentDirectory.childDirectories.find { it.dirName == param }
+      ?: Directory(dirName = param, path = "${currentDirectory.path}$param")
+  }
+
+  fun String.setRoot(): String {
+    return if (isBlank()) {
+      "/"
+    } else {
+      this
+    }
+  }
+
+  fun String.addLeadingSlash(): String {
+    return if (this.first() != '/') {
+      "/" + this
+    } else {
+      this
+    }
+  }
+
+  fun String.addTrailingSlash(): String {
+    return if (this.last() != '/') {
+      this + "/"
+    } else {
+      this
+    }
+  }
+
+  fun String.isCommand() = startsWith("$")
+  fun String.isFile() = first().isDigit()
+
+  fun String.isDir() = startsWith("dir")
+}
+
+data class AdventFile(
+  val fileName: String,
+  val size: Int
+)
+
+data class Directory(
+  val dirName: String,
+  val path: String,
+  val files: MutableList<AdventFile> = mutableListOf(),
+  val childDirectories: MutableList<Directory> = mutableListOf(),
+)
+
+fun main() {
+  NoSpaceLeftPart1().findTotalDirSizeForDirsUnder100k()
+}
